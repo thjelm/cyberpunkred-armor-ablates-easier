a5882c50f95c6e4323f12d1b589e38afa29aa7dc
Thomas Hjelm <thomas@azabua.com>
2024-08-15 01:42:12 -0500

    some changes to get the config options to load correctly in foundry

--- scripts/config.js
+++ scripts/config.js
@@ -1,22 +1,25 @@
 export class Config {
     static registerSettings() {
         console.log("cyberpunkred-armor-ablates-easier settings start");
+
         game.settings.register("cyberpunkred-armor-ablates-easier", "house-rule-enable", {
-        name: game.i18n.localize("cyberpunkred-armor-ablates-easier.house-rule-enable.name"),
-        hint: game.i18n.localize("cyberpunkred-armor-ablates-easier.house-rule-enable.hint"),
-        scope: "world",
-        config: true,
-        type: Boolean,
-        default: false,
+            name: game.i18n.localize("cyberpunkred-armor-ablates-easier.settings.house-rule-enable.name"),
+            hint: game.i18n.localize("cyberpunkred-armor-ablates-easier.settings.house-rule-enable.hint"),
+            scope: "world",
+            config: true,
+            type: Boolean,
+            default: false,
         });
+
         game.settings.register("cyberpunkred-armor-ablates-easier", "armor-sp-ignore-threshold", {
-            name: game.i18n.localize("cyberpunkred-armor-ablates-easier.armor-sp-ignore-threshold.name"),
-            hint: game.i18n.localize("cyberpunkred-armor-ablates-easier.armor-sp-ignore-threshold.hint"),
-        scope: "world",
-        config: true,
-        type: Int,
-        default: 13,
+            name: game.i18n.localize("cyberpunkred-armor-ablates-easier.settings.armor-sp-ignore-threshold.name"),
+            hint: game.i18n.localize("cyberpunkred-armor-ablates-easier.settings.armor-sp-ignore-threshold.hint"),
+            scope: "world",
+            config: true,
+            type: Number,
+            default: 13,
         });
+
         console.log("cyberpunkred-armor-ablates-easier settings end");
   }
 }
